128418d47c76e494098331442b127b1b3f49d1ae
laijunlinfz <993700855@qq.com>
2021-12-09 20:53:52 +0800

    style: rename

--- src/main/java/com/ssmp/utils/ProjectExceptionAdvice.java
+++ src/main/java/com/ssmp/utils/ProjectExceptionAdvice.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ProjectExceptionAdvice {
     // token失效
     @ExceptionHandler(NotLoginException.class)
-    public R doException1(Exception ex) {
+    public R handleNotLoginException(Exception ex) {
         //记录日志
         //通知运维
         //通知开发
@@ -23,7 +23,7 @@ public class ProjectExceptionAdvice {
 
     // 拦截所有异常
     @ExceptionHandler(Exception.class)
-    public R doException(Exception ex) {
+    public R handleException(Exception ex) {
         //记录日志
         //通知运维
         //通知开发
