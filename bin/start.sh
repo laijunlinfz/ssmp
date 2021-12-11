c5f37a050df223ee573fc149ef05a84f2dcd3533
laijunlinfz <993700855@qq.com>
2021-12-11 15:31:53 +0800

    feat: 部署脚本测试1

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,16 @@
+cd /usr/local/product/project/ssmpdeploy
+
+docker stop ssmpcontainer
+docker rm ssmpcontainer
+docker rmi ssmpimage
+
+cd /usr/local/product/project/ssmp
+git pull
+mvn clean package
+
+\cp /usr/local/product/project/ssmp/bin/Dockerfile /usr/local/product/project/ssmpdeploy/Dockerfile
+\cp /usr/local/product/project/ssmp/target/ssmp.jar /usr/local/product/project/ssmpdeploy/ssmp.jar
+
+cd /usr/local/product/project/ssmpdeploy
 docker build -t ssmpimage .
 docker run --name ssmpcontainer -d -p 8080:8080 ssmpimage
